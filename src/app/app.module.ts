bcfee5efc04b104b25bddc3fac2a503a970bc57d
Jonas Américo <jonasamericod@gmail.com>
2019-10-27 15:58:10 -0300

    ngSwitch
    e Inicio do ngTemplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgStyleComponent } from './ng-style/ng-style.component';
 import { FormsModule } from '@angular/forms';
-import { MatSelectModule, MatRadioModule, MatCardModule, MatCheckboxModule, MatInputModule, MatListModule, MatIconModule, MatDividerModule, MatButtonModule } from '@angular/material';
+import { MatSelectModule, MatRadioModule, MatCardModule, MatCheckboxModule, MatInputModule, MatListModule, MatIconModule, MatDividerModule, MatButtonModule, MatProgressBar, MatProgressBarModule } from '@angular/material';
 import { NgClassComponent } from './ng-class/ng-class.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
 import { SubNgIfComponent } from './ng-if/sub-ng-if/sub-ng-if.component';
 import { NgForComponent } from './ng-for/ng-for.component';
 import { NgForFormComponent } from './ng-for-form/ng-for-form.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
+import { NgTemplateComponent } from './ng-template/ng-template.component';
 
 
 @NgModule({
@@ -21,7 +23,9 @@ import { NgForFormComponent } from './ng-for-form/ng-for-form.component';
     NgIfComponent,
     SubNgIfComponent,
     NgForComponent,
-    NgForFormComponent
+    NgForFormComponent,
+    NgSwitchComponent,
+    NgTemplateComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +39,8 @@ import { NgForFormComponent } from './ng-for-form/ng-for-form.component';
     MatListModule,
     MatIconModule,
     MatDividerModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
